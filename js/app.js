11c9c22447f626b3ee8f6c769935f2a00530bcd8
Anthony Eriksen <ajrayid@gmail.com>
2019-03-31 14:18:55 -0700

    Added charts, grid, flexbox and some jquery
    Finally figured out charts.js. Got the header and nav grid/flex box where I want it for mobile so far.
    
    Still big work in progress.

---
+++ js/app.js
@@ -0,0 +1,118 @@
+const dashboardContent = document.getElementsByTagName('main')
+const dashboardIcon = document.getElementById('dashboardIcon');
+
+$(document).ready(function() {
+  $("#dashboardIcon").click(function () {
+  $(".mainContent").toggle()
+  });
+  });
+
+
+
+
+
+
+// Line Chart
+new Chart(document.getElementById("lineChart"), {
+    type: 'line',
+    data: {
+      labels: ['16-22','23-29','30-5','6-12','13-19','20-26','27-3','4-10','11-17','18-24','25-31'],
+      datasets: [{ 
+          data: [4,7,5,9,12,9,10,7,10,13,10,13],
+          label: "Hourly",
+          borderColor: "rgba(20, 3, 80, 0.76)",
+          backgroundColor: "rgba(111, 88, 211, 0.325)",
+          borderWidth: .7,
+          fill: true
+        }, { 
+          data: [107,178,128,214,285,214,250,178,250,321,250,321],
+          label: "Daily",
+          borderColor: "rgba(20, 3, 80, 0.76)",
+          backgroundColor: "rgba(111, 88, 211, 0.325)",
+          borderWidth: .7,
+          fill: true
+        }, { 
+          data: [750,1250,900,1500,2000,1500,1750,1250,1750,2250,1750,2250],
+          label: "Weekly",
+          borderColor: "rgba(20, 3, 80, 0.76)",
+          backgroundColor:"rgba(111, 88, 211, 0.325)",
+          borderWidth: .7,
+          fill: true
+        }, { 
+          data: [22500,37500,27000,45000,60000,45000,52500,37500,52500,67500,52500,67500],
+          label: "Monthly",
+          borderColor: "rgba(20, 3, 80, 0.76)",
+          backgroundColor:"rgba(111, 88, 211, 0.325)",
+          borderWidth: .7,
+          fill: true
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'TRAFFIC'
+      }
+    }
+  });
+
+// Bar Chart
+new Chart(document.getElementById("barChart"), {
+    type: 'bar',
+    data: {
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat'],
+        datasets: [{
+            label: 'DAILY TRAFFIC',
+            data: [75, 100, 175, 125, 225, 200, 100],
+            backgroundColor: [
+                'rgba(46, 32, 107, 0.900)',
+                'rgba(46, 32, 107, 0.900)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)'
+            ],
+            borderColor: [
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)',
+                'rgba(46, 32, 107, 0.905)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+new Chart(document.getElementById("doughtnutChart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Phones", "Tablets", "Desktop"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f"],
+          data: []
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'MOBILE USERS'
+      }
+    }
+});
+
